25490573724bd0129a2a9e0c4f572391cf9c411b
kitty_aleks <kyryllova.aleks@gmail.com>
2019-06-10 01:17:05 +0300

    Add leaflet map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { icon, tileLayer, latLng, marker, Marker } from 'leaflet';
 
 class ParentItem {
   id: number;
   name: string;
   children: ChildItem[];
   opened: boolean;
-
 }
 
 class ChildItem {
   parentId: number;
   name: string;
-
+  markerInstance: Marker;
 }
 
 @Component({
@@ -20,6 +20,15 @@ class ChildItem {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  map;
+  options = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
+    ],
+    zoom: 10,
+    center: latLng(50.442, 30.521)
+  };
+
   // DATA
   data: ParentItem[] =
     [
@@ -27,8 +36,8 @@ export class AppComponent {
         id: 1,
         name: 'Parent 1',
         children: [
-          { name: 'Child 1', parentId: 1 },
-          { name: 'Child 2', parentId: 1 }
+          { name: 'Child 1', parentId: 1, markerInstance: null },
+          { name: 'Child 2', parentId: 1, markerInstance: null }
         ],
         opened: true
       },
@@ -37,8 +46,8 @@ export class AppComponent {
         id: 2,
         name: 'Parent 2',
         children: [
-          { name: 'Child 3', parentId: 2 },
-          { name: 'Child 4', parentId: 2 }
+          { name: 'Child 3', parentId: 2, markerInstance: null },
+          { name: 'Child 4', parentId: 2, markerInstance: null}
         ],
         opened: true
       },
@@ -47,8 +56,8 @@ export class AppComponent {
         id: 3,
         name: 'Parent 3',
         children: [
-          { name: 'Child 5', parentId: 3 },
-          { name: 'Child 6', parentId: 3 }
+          { name: 'Child 5', parentId: 3, markerInstance: null },
+          { name: 'Child 6', parentId: 3, markerInstance: null }
         ],
         opened: true
       },
@@ -57,10 +66,62 @@ export class AppComponent {
   // ACTIVE
   active: ChildItem[] = [];
 
+  constructor(private zone: NgZone) {}
+
   moveToActive(dataItem: ParentItem, childItem: ChildItem): void {
     dataItem.children = dataItem.children.filter(obj => obj !== childItem);
     this.active.push(childItem);
+
+    let marker_ = marker(
+      [
+        this.options.center.lat + this.getRandom(-0.2, 0.2),
+        this.options.center.lng + this.getRandom(-0.2, 0.2)
+      ],
+      {
+        icon: icon({
+          iconSize: [25, 41],
+          iconAnchor: [13, 41],
+          iconUrl: 'leaflet/marker-icon.png',
+          shadowUrl: 'leaflet/marker-shadow.png'
+        }),
+        title: childItem.name
+      }
+    );
+    marker_.addTo(this.map);
+    marker_.on('click', () => {
+      this.zone.run(() => {
+        this.moveToData(childItem);
+      });
+    });
+    childItem.markerInstance = marker_;
   }
+
+  moveAllToActive(dataItem: ParentItem): void {
+    dataItem.children.forEach((childItem) => {
+      this.moveToActive(dataItem, childItem);
+    });
+  }
+
+  moveToData(childItem: ChildItem): void {
+    this.data.forEach(dataItem => {
+      if (dataItem.id === childItem.parentId) {
+        this.active = this.active.filter(obj => obj !== childItem);
+        dataItem.children.push(childItem);
+        childItem.markerInstance.removeFrom(this.map);
+        childItem.markerInstance = null; // освобождаем объект из памяти
+      }
+    });
+  }
+
+  onMapReady(map) {
+    // get a local reference to the map as we need it later
+    this.map = map;
+  }
+
+  getRandom(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
 
   /*  addItem(text: string, price: number): void {
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, LeafletModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
